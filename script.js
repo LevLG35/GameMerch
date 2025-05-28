467b917d7278a67b4dcb113879388288e0f0a60b
LevLG35 <levbychkov2006@gmail.com>
2025-05-28 13:03:55 +0300

    script.js

--- script.js
+++ script.js
@@ -54,3 +54,19 @@ if (checkoutBtn) {
     window.location.href = 'checkout.html';
   });
 }
+
+  function addToCart(productName, price) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    const existingItem = cart.find(item => item.name === productName);
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      cart.push({ name: productName, price: price, quantity: 1 });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    
+    alert(`${productName} додано до кошика!`);
+  }
